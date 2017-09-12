7408d7540ba617c524b3552ef19d8b3be97ff59e
rickard0 <brian.rickard@gmail.com>
2017-09-11 18:12:02 -0700

    'Roll a Ball' tutorial up through item collection

---
+++ Roll a ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player; 
+	private Vector3 offset; 
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position; 	
+	}
+	
+	// LateUpdate is guaranteed to run after all items have been run in Update
+	// Setting the position in LastUpdate means we can be sure the object has moved.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb; // Bad tutorial variable naming. This should be "player", no?
+	public float speed; // This will show up in the inspector as an editable property
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> (); 
+	}
+	
+	// FixedUpdate
+	// Called before any physics calculations are performed
+	// Physics code goes here.
+	void FixedUpdate () 
+	{ 
+		// Gets keyboard inputs 
+		float moveHorizontal = Input.GetAxis ("Horizontal"); 
+		float moveVertical = Input.GetAxis ("Vertical"); 
+
+		rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed); 
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);	
+		}
+	}
+}
