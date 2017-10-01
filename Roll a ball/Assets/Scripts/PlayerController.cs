c1f16fe149db76c4ed21c0b94ea5d84501d1b9a7
rickard0 <brian.rickard@gmail.com>
2017-10-01 09:14:34 -0700

    Roll a Ball beginner tutorial

--- Roll a ball/Assets/Scripts/PlayerController.cs
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb; // Bad tutorial variable naming. This should be "player", no?
 	public float speed; // This will show up in the inspector as an editable property
+	public Text winText;
+	public Text countText;
+
+
+	private int count; 
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> (); 
+		count = 0; 
+		setCountText (); 
+		winText.text = "";
 	}
 	
 	// FixedUpdate
@@ -29,6 +38,26 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up")) 
 		{
 			other.gameObject.SetActive (false);	
+			count++; 
+			setCountText (); 
+
+			if (count >= 12) 
+			{
+				endGame ();
+			}
 		}
+	}
+
+	void setCountText() 
+	{
+		countText.text = "Score: " + count.ToString ();
+	}
+
+	void endGame()
+	{
+		speed = 0;
+		rb.AddForce (new Vector3 (0, 0, 0));
+		this.StopAllCoroutines ();
+		winText.text = "You Win!";
 	}
 }
