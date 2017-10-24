77390b3b004c503d077abfc0ffe000bd738a1302
rickard0 <brian.rickard@gmail.com>
2017-10-23 19:47:33 -0700

    Enemy ship add-on in progress

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -13,10 +13,25 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rb; 
 	public float tilt; 
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
 	public Boundary boundary; 
+
+	private float nextFire;
 
 	void Start() { 
 		rb = GetComponent<Rigidbody> (); 
+	}
+
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 
 	void FixedUpdate() { 
